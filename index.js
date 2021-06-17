660fb23309cd795999645db91426eceb4642fba1
rocioferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2021-06-17 01:12:40 -0300

    something I guess

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const express = require('express')
 const app = express()
+const {promisify} = require('util');
 const cors = require('cors');
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 
 app.use(cors())
-const port = 9898
+const authHost = '10.102.197.244'
+const geoHost = '10.102.197.112'
+const port = 8080
 const options = {
     keepCase: true,
     longs: String,
     enums: String,
     defaults: true,
 }
+
+function promisifyAll(client) {
+    const to = {};
+    for (var k in client) {
+        if (typeof client[k] != 'function') continue;
+        to[k] = promisify(client[k].bind(client));
+    }
+    return to;
+}
+
+const authDefinition = protoLoader.loadSync( __dirname + '/protobuf/auth.proto', options);
+const authObject = grpc.loadPackageDefinition(authDefinition);
+const authClient = promisifyAll(new authObject.AuthService(`${authHost}:${port}`, grpc.credentials.createInsecure()))
+
+const geoDefinition = protoLoader.loadSync( __dirname + '/protobuf/geo.proto', options);
+const geoObject = grpc.loadPackageDefinition(geoDefinition);
+const geoClient = promisifyAll(new geoObject.GeoService(`${geoHost}:${port}`, grpc.credentials.createInsecure()))
+
+app.get('/authService', (req, res) => {
+    res.send(authClient.authenticate({mail: 'juan@gmail.com', password: '1234'}))
+})
+
 app.get('/geoService', (req, res) => {
-    const packageDefinition = protoLoader.loadSync( __dirname + 'protobuf/geo.proto', options);
-    const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
-    new packageObject.GeoService(host + ':' + port, grpc.credentials.createInsecure())
+    res.send(geoClient.getCountryCityByIP({ip: "8.8.8.8"}))
+})
+
+app.get('/', (req, res) => {
+    res.send('/')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.get('/', (req, res) => {
-    res.send('/')
-})
